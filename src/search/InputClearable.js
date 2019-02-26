cd4e13acc4e67cc284b520338e923becbe48fce3
nikrb <nik_rb@yahoo.com>
2019-02-26 23:43:24 +0000

    focus search input after clear

--- src/search/InputClearable.js
+++ src/search/InputClearable.js
@@ -2,18 +2,34 @@ import React from 'react';
 
 import { ClearButton, Input, InputWrapper } from './styled';
 
-const InputClearable = ({ onChange, onClear, text }) =>
-  <InputWrapper>
-    <Input
-      autoFocus
-      placeholder="title"
-      type="text"
-      onChange={onChange}
-      onKeyDown={onChange}
-      value={text}
-    />
-    <ClearButton onClick={onClear}>x</ClearButton>
-  </InputWrapper>;
+class InputClearable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.ipRef = React.createRef();
+    this.onClear = this.onClear.bind(this);
+  }
+  onClear(e) {
+    this.props.onClear();
+    this.ipRef.current.focus();
+  }
+  render() {
+    const { onChange, text } = this.props;
+    return (
+      <InputWrapper>
+        <Input
+          autoFocus
+          placeholder="title"
+          type="text"
+          onChange={onChange}
+          onKeyDown={onChange}
+          value={text}
+          ref={this.ipRef}
+        />
+        <ClearButton onClick={this.onClear}>x</ClearButton>
+      </InputWrapper>
+    );
+  }
+}
 
 export default InputClearable;
 
